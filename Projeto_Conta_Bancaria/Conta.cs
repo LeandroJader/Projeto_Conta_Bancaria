25e1032ecf6b234d281501887a7d7d4f621605bf
leandroJader <ldandro>
2025-04-03 10:17:55 -0300

    implementado objeto conta juntamente com algumas funcoes para lógica de funcionamento do projeto.

---
+++ Projeto_Conta_Bancaria/Conta.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Projeto_Conta_Bancaria;
+
+public class Conta
+{
+    public int Id_Conta;
+    public decimal Saldo;
+    public decimal Limite_Debito;
+    public decimal saque;
+    public decimal deposito;
+    public string[] extrato;
+
+
+    public Conta(int id_Conta, decimal saldo, decimal limite_Debito)
+    {
+        Id_Conta = id_Conta;
+        Saldo = saldo;
+        Limite_Debito = limite_Debito;
+
+    }
+
+    public void Sacar(decimal saque)
+    {
+        {
+
+            //sacar
+
+            if (saque > Limite_Debito)
+                Console.WriteLine("o valor nao esta disponivel");
+            else
+            {
+                Saldo -= saque;
+                extrato = [($"saldo:  {Saldo} - Saque: {saque}")];
+            }
+
+        }
+
+    }
+    public void depositar(decimal deposito)
+    {
+
+        Saldo += deposito;
+        extrato=[($"Depósito: +{deposito} | Saldo Atual: {Saldo}")];
+
+    }
+
+    public void ConsultarSaldo(decimal saldo)
+    {
+        Console.WriteLine(saldo);
+    }
+
+    public void transferirSaldo(Conta conta2, decimal valor)
+    {
+        Saldo -= valor;
+        conta2.Saldo += valor;
+       
+
+    }
+
+
+}
--- Projeto_Conta_Bancaria/Program.cs
+++ Projeto_Conta_Bancaria/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Projeto_Conta_Bancaria
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+
+namespace Projeto_Conta_Bancaria
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Conta conta1 = new Conta(1,1500,700);
+            Conta conta2 = new Conta(2, 100, 700);
+
+            conta1.Sacar(200);
+            
+            conta1.depositar(100);
+
+            conta1.ConsultarSaldo(conta1.Saldo);
+
+
+            conta1.transferirSaldo(conta2,200);
+            Console.WriteLine($"o saldo da conta agora e de  {conta2.Saldo}");
+            Console.ReadLine();
+    }
+      
+
+
     }
 }
