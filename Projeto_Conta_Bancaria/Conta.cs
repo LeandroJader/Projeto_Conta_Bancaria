f7111c4b4704f6392fceaffc41dc8e1ae48fb39d
leandroJader <ldandro>
2025-04-03 11:03:24 -0300

    implementada  melhorias no funcionamento das funções  juntamente com melhorias de interface do terminal

--- Projeto_Conta_Bancaria/Conta.cs
+++ Projeto_Conta_Bancaria/Conta.cs
@@ -1,62 +1,70 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
 
-namespace Projeto_Conta_Bancaria;
-
-public class Conta
+namespace Projeto_Conta_Bancaria
 {
-    public int Id_Conta;
-    public decimal Saldo;
-    public decimal Limite_Debito;
-    public decimal saque;
-    public decimal deposito;
-    public string[] extrato;
-
-
-    public Conta(int id_Conta, decimal saldo, decimal limite_Debito)
+    public class Conta
     {
-        Id_Conta = id_Conta;
-        Saldo = saldo;
-        Limite_Debito = limite_Debito;
+        public int Id_Conta { get; private set; }
+        public decimal Saldo { get; private set; }
+        public decimal Limite_Debito { get; private set; }
+        private List<string> extrato;
 
-    }
-
-    public void Sacar(decimal saque)
-    {
+        public Conta(int id_Conta, decimal saldo, decimal limite_Debito)
         {
-
-            //sacar
-
-            if (saque > Limite_Debito)
-                Console.WriteLine("o valor nao esta disponivel");
-            else
-            {
-                Saldo -= saque;
-                extrato = [($"saldo:  {Saldo} - Saque: {saque}")];
-            }
-
+            Id_Conta = id_Conta;
+            Saldo = saldo;
+            Limite_Debito = limite_Debito;
+            extrato = new List<string>();
         }
 
+        public void Sacar(decimal valor)
+        {
+            if (valor > Saldo)
+            {
+                Console.WriteLine("Erro: Saldo insuficiente.");
+            }
+            else
+            {
+                Saldo -= valor;
+                extrato.Add($"Débito de R$ {valor:F2}");
+                Console.WriteLine("Saque realizado com sucesso!");
+            }
+        }
+
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+            extrato.Add($"Crédito de R$ {valor:F2}");
+            Console.WriteLine("Depósito realizado com sucesso!");
+        }
+
+        public void Transferir(Conta destino, decimal valor)
+        {
+            if (valor > Saldo)
+            {
+                Console.WriteLine("Erro: Saldo insuficiente para transferência.");
+            }
+            else
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                extrato.Add($"Transferência de R$ {valor:F2} para Conta {destino.Id_Conta}");
+                Console.WriteLine("Transferência realizada com sucesso!");
+            }
+        }
+
+        public void ExibirExtrato()
+        {
+            Console.WriteLine($"\nExtrato da Conta #{Id_Conta}");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Movimentações:");
+            foreach (var item in extrato)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine($"Saldo atual: R$ {Saldo:F2}\n");
+        }
     }
-    public void depositar(decimal deposito)
-    {
-
-        Saldo += deposito;
-        extrato=[($"Depósito: +{deposito} | Saldo Atual: {Saldo}")];
-
-    }
-
-    public void ConsultarSaldo(decimal saldo)
-    {
-        Console.WriteLine(saldo);
-    }
-
-    public void transferirSaldo(Conta conta2, decimal valor)
-    {
-        Saldo -= valor;
-        conta2.Saldo += valor;
-       
-
-    }
-
-
 }
--- Projeto_Conta_Bancaria/Program.cs
+++ Projeto_Conta_Bancaria/Program.cs
@@ -1,5 +1,4 @@
-﻿using static System.Net.Mime.MediaTypeNames;
-using System.Drawing;
+﻿using System;
 
 namespace Projeto_Conta_Bancaria
 {
@@ -7,22 +6,105 @@ namespace Projeto_Conta_Bancaria
     {
         static void Main(string[] args)
         {
-            Conta conta1 = new Conta(1,1500,700);
-            Conta conta2 = new Conta(2, 100, 700);
+            Conta conta1 = new Conta(12, 1000, 500);
+            Conta conta2 = new Conta(13, 700, 500);
 
-            conta1.Sacar(200);
-            
-            conta1.depositar(100);
+            while (true)
+            {
+                Console.WriteLine("\nEscolha uma opção:");
+                Console.WriteLine("1 - Sacar");
+                Console.WriteLine("2 - Depositar");
+                Console.WriteLine("3 - Consultar Saldo");
+                Console.WriteLine("4 - Transferir");
+                Console.WriteLine("5 - Exibir Extrato");
+                Console.WriteLine("6 - Sair");
+                Console.Write("Opção: ");
 
-            conta1.ConsultarSaldo(conta1.Saldo);
+                int opcao = Convert.ToInt32(Console.ReadLine());
 
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Digite o valor do saque: ");
+                        decimal saque = Convert.ToDecimal(Console.ReadLine());
+                        Console.Write("Escolha a conta (1 ou 2): ");
+                        int escolhaSaque = Convert.ToInt32(Console.ReadLine());
 
-            conta1.transferirSaldo(conta2,200);
-            Console.WriteLine($"o saldo da conta agora e de  {conta2.Saldo}");
-            Console.ReadLine();
-    }
-      
+                        if (escolhaSaque == 1)
+                            conta1.Sacar(saque);
+                        else if (escolhaSaque == 2)
+                            conta2.Sacar(saque);
+                        else
+                            Console.WriteLine("Conta inválida!");
 
+                        break;
 
+                    case 2:
+                        Console.Write("Digite o valor do depósito: ");
+                        decimal deposito = Convert.ToDecimal(Console.ReadLine());
+                        Console.Write("Escolha a conta (1 ou 2): ");
+                        int escolhaDeposito = Convert.ToInt32(Console.ReadLine());
+
+                        if (escolhaDeposito == 1)
+                            conta1.Depositar(deposito);
+                        else if (escolhaDeposito == 2)
+                            conta2.Depositar(deposito);
+                        else
+                            Console.WriteLine("Conta inválida!");
+
+                        break;
+
+                    case 3:
+                        Console.Write("Escolha a conta para consultar saldo (1 ou 2): ");
+                        int escolhaSaldo = Convert.ToInt32(Console.ReadLine());
+
+                        if (escolhaSaldo == 1)
+                            Console.WriteLine($"Saldo da Conta #12: R$ {conta1.Saldo:F2}");
+                        else if (escolhaSaldo == 2)
+                            Console.WriteLine($"Saldo da Conta #13: R$ {conta2.Saldo:F2}");
+                        else
+                            Console.WriteLine("Conta inválida!");
+
+                        break;
+
+                    case 4:
+                        Console.Write("Digite o valor para transferência: ");
+                        decimal valorTransferencia = Convert.ToDecimal(Console.ReadLine());
+
+                        Console.Write("Transferir de qual conta? (1 ou 2): ");
+                        int contaOrigem = Convert.ToInt32(Console.ReadLine());
+
+                        if (contaOrigem == 1)
+                            conta1.Transferir(conta2, valorTransferencia);
+                        else if (contaOrigem == 2)
+                            conta2.Transferir(conta1, valorTransferencia);
+                        else
+                            Console.WriteLine("Conta inválida!");
+
+                        break;
+
+                    case 5:
+                        Console.Write("Escolha a conta para exibir o extrato (1 ou 2): ");
+                        int escolhaExtrato = Convert.ToInt32(Console.ReadLine());
+
+                        if (escolhaExtrato == 1)
+                            conta1.ExibirExtrato();
+                        else if (escolhaExtrato == 2)
+                            conta2.ExibirExtrato();
+                        else
+                            Console.WriteLine("Conta inválida!");
+
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Saindo...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            }
+        }
     }
 }
